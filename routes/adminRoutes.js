60a69b47446c37a20236c7e952fd23e1d20144b3
clinto92 <clinto92@gmail.com>
2021-05-23 01:13:23 +0530

    cors issues fixed & heroku start script assigned node in package.json

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 const router = express.Router();
 import {
@@ -9,19 +10,25 @@ import {
   getTeacherInfo,
 } from "../controllers/adminControl.js";
 
+const corsOptions = {
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204,
+}
 // import auth from "../middleware/auth.js";
 // admin
-router.post("/admin-signin", signin);
-router.get("/admin-info", getAdminInfo);
-router.patch("/update-admin/:id");
+router.post("/admin-signin", cors(corsOptions), signin);
+router.get("/admin-info", cors(corsOptions), getAdminInfo);
+router.patch("/update-admin/:id", cors(corsOptions));
 // teacher
-router.post("/teacher-signup", signupTeacher);
-router.get("/teachers-info", getTeacherInfo);
-router.patch("/update-teacher/:id");
-router.delete("/delete-teacher/:id");
+router.post("/teacher-signup", cors(corsOptions), signupTeacher);
+router.get("/teachers-info", cors(corsOptions), getTeacherInfo);
+router.patch("/update-teacher/:id", cors(corsOptions));
+router.delete("/delete-teacher/:id", cors(corsOptions));
 // student
-router.post("/create-student", createPost);
-router.get("/students-info", getPosts);
-router.patch("/update-student/:id");
-router.delete("/delete-student/:id");
+router.post("/create-student", cors(corsOptions), createPost);
+router.get("/students-info", cors(corsOptions), getPosts);
+router.patch("/update-student/:id", cors(corsOptions));
+router.delete("/delete-student/:id", cors(corsOptions));
 export default router;
--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
+import cors from "cors";
 const router = express.Router();
 
 import { signin, getTeacherInfo } from "../controllers/teacherControl.js";
+const corsOptions = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204,
+  }
 
-router.post("/signin", signin);
-router.get("/teachers-info", getTeacherInfo);
+router.post("/signin", cors(corsOptions), signin);
+router.get("/teachers-info", cors(corsOptions), getTeacherInfo);
 
 export default router;
